5ecaa0c8d911cc06f767d024b785dfbec1cddd7d
Juanma González <juangamgm@gmail.com>
2026-03-12 09:26:38 +0100

    init

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN: str = os.getenv('DISCORD_TOKEN', 'no_token_found')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+    
+bot.run(TOKEN)
